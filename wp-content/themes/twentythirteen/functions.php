86275b00a73f5dead4ef6e3ac84a6edb2b179c84
Sam <sam@sphericalcommunications.com>
2013-10-19 13:34:21 -0400

    fixed the custom post type code for products

--- wp-content/themes/twentythirteen/functions.php
+++ wp-content/themes/twentythirteen/functions.php
@@ -527,6 +527,26 @@ add_action( 'customize_preview_init', 'twentythirteen_customize_preview_js' );
 
 
 
+/**
+ * added this code to create a custom post type
+ *
+ */
+
+add_action( 'init', 'prowp_register_my_post_types' );
+function prowp_register_my_post_types() {
+	
+	register_post_type( 'products',
+		array(
+			'labels' => array(
+				'name' => 'Products'
+				),
+				'public' => true,
+				
+			)
+		);
+}
+
+
 
 
 
